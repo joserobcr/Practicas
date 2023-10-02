4b80d1481e5b1007443b8508962c35e2830a1414
joserobcr <joserobcr2003@hotmail.com>
2023-10-02 18:55:34 -0500

    Se agrego un nuevo documento

---
+++ Parcial 2/Modulo Ecma/Areas.js
@@ -0,0 +1,18 @@
+export function areacuadrado(lado){
+    return lado*lado;
+}
+
+export function areatriangulo(base, altura){
+    return ((base*altura)/2);
+}
+
+export function suma(num1, num2){
+    return num1+num2;
+}
+export function resta(num1, num2){
+    return num1-num2;
+}
+export function multiplicacion(num1, num2){
+    return num1*num2;
+}
+
---
+++ Parcial 2/Modulo Ecma/CalculoAreas.js
@@ -0,0 +1,16 @@
+import * as areas from "./Areas.js";
+let areas = require("./Areas.js");
+let a=areas.areacuadrado(4);
+console.log(a);
+
+let t=areas.areatriangulo(3,6);
+console.log(t);
+
+let s=areas.suma(5,32);
+console.log(s);
+
+let r=areas.resta(23,12);
+console.log(r);
+
+let m=areas.multiplicacion(7,6);
+console.log(m);
