cffd891908a15d4935c92d74e68482fa17f26f2b
joserobcr <joserobcr2003@hotmail.com>
2023-10-16 10:35:49 -0500

    Se agrego un documeno

---
+++ Parcial 2/EjemploCallBacks/EjemploCallBacks.js
@@ -0,0 +1,9 @@
+setTimeout(function(){
+    console.log("El temporizador ha llegado a cero...")
+}, 1000);
+
+let mensaje = function(){
+    console.log("Otro mensaje...");
+};
+
+setTimeout(mensaje, 3500);
