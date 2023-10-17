6fb52253a46e2b768c1449d3153cdd7c969ff6b5
joserobcr <joserobcr2003@hotmail.com>
2023-10-17 07:35:18 -0500

    Se modifico un documento

--- Parcial 2/EjemploCallBacks/EjemploCallBacks.js
+++ Parcial 2/EjemploCallBacks/EjemploCallBacks.js
@@ -1,9 +1,9 @@
 setTimeout(function(){
     console.log("El temporizador ha llegado a cero...")
-}, 1000);
+}, 9000);
 
 let mensaje = function(){
     console.log("Otro mensaje...");
 };
 
-setTimeout(mensaje, 3000);
+setTimeout(mensaje, 15000);
