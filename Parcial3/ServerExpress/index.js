52b362878dad4282da6207cd1fe3a42673d7bb5b
joserobcr <joserobcr2003@hotmail.com>
2023-11-20 20:37:23 -0600

    Se modifico documento

--- Parcial3/ServerExpress/index.js
+++ Parcial3/ServerExpress/index.js
@@ -5,7 +5,6 @@ const app = express();
 
 app.use(cors());
 
-//create connection database
 const connection = mysql2.createConnection({
     host: 'localhost',
     port:'3306',
@@ -63,60 +62,3 @@ app.delete('/',(req,res)=>{//alta
 app.listen(8085,(req,res)=>{
     console.log("Servidor express corriendo en  puerto 8082")
 });
-//------------------------
-
-/*const express = require ("express");
-const app= express();
-const mysql = require("mysql2");
-const cors = cors('cors');
-
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'Alumnos'
-  });
-
-app.get('/alumnos',(req,res)=>{
-    console.log(req.query.idUsuario);
-
-    let consulta=''
-    if(typeof(req.query.idUsuario)=='undefined')
-    {
-        consulta='select * from alumnos'
-    }
-    else{
-        consulta='select * from usuario where idAlumnos'
-    }
-    console.log(consulta);
-    connection.query(
-        'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
-        function(err, results, fields) {
-          if(results.length==0)
-          {
-            res.json(results);
-          }else{
-            res.json(results);
-          }
-        
-          res.json(results);
-        }
-      );
-})
-
-app.use(cors());
-
-app.get('/',(req,res)=>{
-    res.json({ mensaje: "Server Express respondiendo a get" });
-});
-
-app.post('/',(req,res)=>{
-    resizeTo.json({ mensaje: "Server Express respondiendo a post" });
-});
-
-app.delete('/', (req,res)=>{
-    resizeTo.json({mensaje:"Server express respondiendo a delete"});
-})
-
-app.listen(8082, ()=>{
-    console.log("Servidor Express en puerto 8082")
-})*/
