880bca042f170b26800964ca0c40f3aac16deed2
joserobcr <joserobcr2003@hotmail.com>
2023-11-23 11:39:08 -0600

    Se agrego nuevo documento

--- Parcial3/ServerExpress/index.js
+++ Parcial3/ServerExpress/index.js
@@ -20,11 +20,11 @@ connection.connect(function(error){
     }
 })
 
-app.get('/canciones',(req,res)=>{//consulta en el diagonal el nombre de la tabla
-    console.log(req.query.idAlumnos);
+/*app.get('/canciones',(req,res)=>{
+    console.log(req.query.Top);
 
     let consulta=''
-    if(typeof(req.query.idAlumnos)==='undefined')
+    if(typeof(req.query.Top)==='undefined')
     {
         consulta='select * from canciones'
     }
@@ -36,10 +36,40 @@ app.get('/canciones',(req,res)=>{//consulta en el diagonal el nombre de la tabla
     connection.query(
         consulta,
         function(err, results, fields){
-            //console.log(results);
-            //console.log(fields);
+            if(results.length==0)
+          {
+            res.json({
+                status:0,
+                mensaje: "Top no existe",
+                datos:{}
+            });
+          }else{
+            res.json({
+                status:1,
+                mensaje: "Usuario encontrado",
+                datos:results[0]
+            });
+          }
+        
+        }
+    )
+});*/
+app.get('/canciones',(req,res)=>{
+    console.log(req.query.Top);
 
-            //console.log(results);//array de objetos
+    let consulta=''
+    if(typeof(req.query.Top)==='undefined')
+    {
+        consulta='select * from canciones'
+    }
+    else{
+        consulta=`select * from canciones where Top=${req.query.Top}`
+    }
+    console.log(consulta)
+
+    connection.query(
+        consulta,
+        function(err, results, fields){
             if(results.length==0)
           {
             res.json({mensaje:"Top no existe"});
@@ -57,6 +87,10 @@ app.post('/',(req,res)=>{//alta
 
 app.delete('/',(req,res)=>{//alta
     res.json({ mensaje:"Servidor Express respondiendo a delete"});
+});
+
+app.patch('/',(req,res)=>{//alta
+    res.json({ mensaje:"Servidor Express respondiendo a patch"});
 });
 
 app.listen(8085,(req,res)=>{
