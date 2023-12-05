cae4f8cfeefe2c92afcb2c2f2a879130e78c517e
joserobcr <joserobcr2003@hotmail.com>
2023-12-05 07:14:17 -0600

    Se modifico Peticion

--- Parcial3/ServerExpress/index.js
+++ Parcial3/ServerExpress/index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const cors = require('cors')
 const mysql2 = require('mysql2/promise');
 const bodyParser = require('body-parser');
-//const { default: jsPDF } = require('jspdf');
+const { default: jsPDF } = require('jspdf');
+const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
-
-const { jsPDF } = require('jspdf');
 
 const multer = require('multer');
 const upload = multer();
@@ -143,116 +142,18 @@ app.put('/canciones', async (req, res) => {
     }   
 });
 
-app.get('/canciones/formato', async(req,res)=>{
-    const Top = req.body.Top || '';
-
-    if (!Top) {
-        return res.status(400).json({ error: "Parámetro 'Top' faltante en la consulta" });
-    }
-
-    if (isNaN(Top)) {
-        return res.status(400).json({ error: "El valor de 'Top' debe ser un número" });
-    }
-
-    try {
-        const connection = await mysql2.createConnection({
-            host: 'localhost',
-            port: '3306',
-            user: 'root',
-            password: 'neymar2003',
-            database: 'Canciones'
-        });
-
-        const [rows, fields] = await connection.execute('SELECT * FROM canciones WHERE Top = ?', [Top]);
-        
-        await connection.end();
-
-        if (rows.length === 0) {
-            return res.status(404).json({ error: "La canción no fue encontrada" });
-        }
-
-        const doc = new jsPDF();
-        doc.setFontSize(12);
-
-        const nombre = rows[0].nombre || '';
-        const artistas = rows[0].artistas || '';
-        const duracion = rows[0].duracion || '';
-
-        doc.text(`Canción:`, 10, 10);
-        doc.text(`Top: ${Top}`, 10, 20);
-        doc.text(`Nombre: ${nombre}`, 10, 30);
-        doc.text(`Artistas: ${artistas}`, 10, 40);
-        doc.text(`Duración: ${duracion}`, 10, 50);
-
-        const filePath = path.join(__dirname, 'Cancion.pdf');
-        doc.save(filePath);
-
-        res.sendFile(filePath, (err) => {
-            if (err) {
-                console.error(err);
-                res.sendStatus(500);
-            }
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Error al procesar la solicitud" });
-    }
-    
-    /*const Top = req.query.Top || '';
-
-    if (!Top) {
-        return res.status(400).json({ error: "Parámetro 'Top' faltante en la consulta" });
-    }
-
-    if (isNaN(Top)) {
-        return res.status(400).json({ error: "El valor de 'Top' debe ser un número" });
-    }
-
-    try {
-        const connection = await mysql2.createConnection({
-            host: 'localhost',
-            port: '3306',
-            user: 'root',
-            password: 'neymar2003',
-            database: 'Canciones'
-        });
-
-        const [rows, fields] = await connection.execute('SELECT * FROM canciones WHERE Top = ?', [Top]);
-        
-        await connection.end();
-
-        if (rows.length === 0) {
-            return res.status(404).json({ error: "La canción no fue encontrada" });
-        }
-
-        const doc = new jsPDF();
-        doc.setFontSize(12);
-
-        const nombre = rows[0].nombre || '';
-        const artistas = rows[0].artistas || '';
-        const duracion = rows[0].duracion || '';
-
-        doc.text(`Canción:`, 10, 10);
-        doc.text(`Top: ${Top}`, 10, 20);
-        doc.text(`Nombre: ${nombre}`, 10, 30);
-        doc.text(`Artistas: ${artistas}`, 10, 40);
-        doc.text(`Duración: ${duracion}`, 10, 50);
-
-        const filePath = path.join(__dirname, 'Cancion.pdf');
-        doc.save(filePath);
-
-        res.sendFile(filePath, (err) => {
-            if (err) {
-                console.error(err);
-                res.sendStatus(500);
-            }
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({ error: "Error al procesar la solicitud" });
-    }*/
+app.post('/canciones/formato', upload.none(), (req, res) => {
+    const doc = new jsPDF();
+    doc.text(`Cancion:`, 10, 10);
+    doc.text(`Top: ${req.body.Top}`, 10, 20);
+    doc.text(`nombre: ${req.body.nombre}`, 10, 30);
+    doc.text(`artistas: ${req.body.artistas}`, 10, 40);
+    doc.text(`duracion: ${req.body.duracion}`, 10, 50);
+    let archivoPDF = path.join(__dirname, 'Archivo.pdf');
+    doc.save(archivoPDF);
+    res.sendFile(archivoPDF);
 });
 
-app.listen(8083,(req,res)=>{
-    console.log("Servidor express corriendo en  puerto 8083")
+app.listen(8080,(req,res)=>{
+    console.log("Servidor express corriendo en  puerto 8080")
 });
